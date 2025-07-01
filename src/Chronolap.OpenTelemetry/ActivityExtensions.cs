fb1163e29c2bf2f7f1fe12d167b76b37d9d8006e
ertugrulkra <ertugrulkra@gmail.com>
2025-07-01 12:45:27 +0300

    OpenTelemetry Integration completed.

---
+++ src/Chronolap.OpenTelemetry/ActivityExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Chronolap;
+using System.Diagnostics;
+
+namespace Choronolap.OpenTelemetry
+{
+    public static class ActivityExtensions
+    {
+        public static void Lap(this Activity? activity, string lapName, ChronolapTimer timer, string? tagPrefix = "lap")
+        {
+            if (activity == null || !activity.IsAllDataRequested)
+                return;
+
+            var lap = timer.Laps.LastOrDefault(l => l.Name == lapName);
+            if (lap == null)
+                return;
+
+            var key = $"{tagPrefix}.{lap.Name.Replace(" ", "_")}.duration_ms";
+            var value = lap.Duration.TotalMilliseconds;
+
+            activity.SetTag(key, value);
+        }
+
+        public static void ExportAllLaps(this Activity? activity, ChronolapTimer timer, string? tagPrefix = "lap")
+        {
+            if (activity == null || !activity.IsAllDataRequested)
+                return;
+
+            foreach (var lap in timer.Laps)
+            {
+                var key = $"{tagPrefix}.{lap.Name.Replace(" ", "_")}.duration_ms";
+                activity.SetTag(key, lap.Duration.TotalMilliseconds);
+            }
+
+            activity.SetTag($"{tagPrefix}.total_ms", timer.TotalLapTime.TotalMilliseconds);
+        }
+    }
+}
